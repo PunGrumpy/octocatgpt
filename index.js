2ecfc2b84905291ebcd7be4f46f96cc8edc3fb06
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-13 01:25:26 +0700

    :bug: fix build error

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const handleIssueOpened = require('./handlers/issuesHandler')
-const handlePullRequestOpenedAndSynchronized = require('./handlers/pullRequestHandler')
-const logger = require('./logger')
+const logger = require('./src/logger/index')
+const handleIssueOpened = require('./src/handlers/issuesHandler')
+const handlePullRequestOpenedAndSynchronized = require('./src/handlers/pullRequestHandler')
 
 module.exports = app => {
-  logger(app).info(`App is running on ${process.env.NODE_ENV} mode`)
+  logger(app).info("Bot's running!")
 
   app.on('issues.opened', handleIssueOpened)
   app.on('pull_request.opened', handlePullRequestOpenedAndSynchronized)
