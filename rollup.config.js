8e9deadd2cdf6342f9b25e754e87011e7b53b995
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-13 01:12:19 +0700

    :package: api for vercel

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
+import json from '@rollup/plugin-json'
+
+export default {
+  input: 'index.js',
+  output: {
+    dir: './dist',
+    format: 'cjs'
+  },
+  plugins: [json(), nodeResolve({ preferBuiltins: true }), commonjs(), terser()]
+}
