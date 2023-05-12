df375099b333950c3cd44cd9182c2fcf7aa0f425
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-13 01:05:43 +0700

    :heavy_plus_sign: add example private key

---
+++ api/github/webhooks/index.js
@@ -0,0 +1,10 @@
+const { createNodeMiddleware, createProbot } = require('probot')
+const app = require('../../../dist/index.js')
+module.exports = createNodeMiddleware(app, {
+  probot: createProbot({
+    overrides: {
+      privateKey: Buffer.from(process.env.PRIVATE_KEY, 'base64').toString()
+    }
+  }),
+  webhooksPath: '/api/github/webhooks'
+})
